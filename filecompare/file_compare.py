7b4c61eb18ffef984930013cd5f23fea80d2507b
IntensiveCoding <intensive.coding@gmail.com>
2016-05-06 19:27:22 +0530

    Initial commits

---
+++ filecompare/file_compare.py
@@ -0,0 +1,20 @@
+# file_compare.py
+#
+# This module is part of FileCompare and is released under
+# the BSD License: http://www.opensource.org/licenses/bsd-license.php
+
+"""FileCompare is an utility to compare files between
+different revisions"""
+
+# import
+
+__author__ = 'Senthil Nayagan'
+__copyright__ = 'Copyright 2016, IntensiveCoding'
+__credits__ = []
+__license__ = 'GPL'
+__version__ = '1'
+__maintainer__ = 'Senthil Nayagan'
+__email__ = 'intensive.coding@gmail.com'
+__status__ = "Development"
+
+
